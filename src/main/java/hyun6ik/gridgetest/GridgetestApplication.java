976c97d38148b026c50953b80a569bccc1e9afa5
hyun6ik <fkanwm@naver.com>
2022-07-26 15:15:36 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/hyun6ik/gridgetest/GridgetestApplication.java
@@ -0,0 +1,13 @@
+package hyun6ik.gridgetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridgetestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridgetestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyun6ik/gridgetest/GridgetestApplicationTests.java
@@ -0,0 +1,13 @@
+package hyun6ik.gridgetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridgetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
