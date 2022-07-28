cb3df80fd00f44c811a218978277a3e6bc09203e
hyun6ik <fkanwm@naver.com>
2022-07-28 16:43:49 +0900

    feat : Auth Annotation 생성

---
+++ src/main/java/hyun6ik/gridgetest/global/annotation/Auth.java
@@ -0,0 +1,12 @@
+package hyun6ik.gridgetest.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Auth {
+
+}
