4fc1d22303250d1b3f7a1166f723c8d94deb6a11
hyun6ik <fkanwm@naver.com>
2022-07-28 22:59:02 +0900

    refactor : PostController url post -> posts로 변경

--- src/main/java/hyun6ik/gridgetest/interfaces/post/controller/PostController.java
+++ src/main/java/hyun6ik/gridgetest/interfaces/post/controller/PostController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/post")
+@RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
 
