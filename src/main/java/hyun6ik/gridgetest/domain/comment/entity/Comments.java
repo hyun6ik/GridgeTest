f07114f5402278492f344e3657b97624e414724a
hyun6ik <fkanwm@naver.com>
2022-07-29 17:29:03 +0900

    feat : Comments post 연관관계 추가

--- src/main/java/hyun6ik/gridgetest/domain/comment/entity/Comments.java
+++ src/main/java/hyun6ik/gridgetest/domain/comment/entity/Comments.java
@@ -4,10 +4,21 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comments {
+
+    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<Comment> comments;
+
+    public Comments(List<Comment> comments) {
+        this.comments = comments;
+    }
 }
