86c86765affd7a3c6a891deee4fb30dbbbed7d3c
hyun6ik <fkanwm@naver.com>
2022-07-31 00:23:07 +0900

    refactor : MemberConstraints CHANGE -> UPDATE로 명칭 변경

--- src/main/java/hyun6ik/gridgetest/interfaces/member/constant/MemberConstraints.java
+++ src/main/java/hyun6ik/gridgetest/interfaces/member/constant/MemberConstraints.java
@@ -2,7 +2,7 @@ package hyun6ik.gridgetest.interfaces.member.constant;
 
 public interface MemberConstraints {
 
-    String CHANGE_PASSWORD = "비밀번호가 설정되었습니다.";
-    String CHANGE_PRIVATE = "비공개 계정으로 전환되었습니다.";
+    String UPDATE_PASSWORD = "비밀번호가 설정되었습니다.";
+    String UPDATE_PRIVATE = "비공개 계정으로 전환되었습니다.";
     String RESIGN_MEMBER = "회원 탈퇴가 완료되었습니다.";
 }
